9b1ab490453997907f788a5c3add828cb320ff8a
jdgregson <jdgregson@gmail.com>
2018-05-10 23:17:51 -0700

    Removed current version of wget
    We need to implement CORS before we can ever utilize that.

--- commands.js
+++ commands.js
@@ -39,21 +39,6 @@ function cat(args) {
     }
 }
 
-
-function wget(args) {
-  let command = document.getElementById('input').value;
-  xhttp = new XMLHttpRequest();
-  xhttp.addEventListener('load', () => {
-    let output = document.createElement('div');
-    output.setAttribute('class', 'output');
-    output.innerHTML = this.responseText;
-    let input = document.getElementById('input-wrap');
-    document.body.insertBefore(output, input);
-    document.getElementById('input').value = prompt;
-  });
-  xhttp.open('GET', BACKEND_URL + encodeURIComponent(command));
-  xhttp.send();
-}let denied_commands = ('vi vim emacs nano cd cat dog sl service systemctl apt '+
 let denied_commands = ('vi vim emacs nano cd cat dog sl service systemctl apt '+
     'apt-get yum make install ./configure su sudo chmod chown del rm nc '+
     'netcat').split(' ');
