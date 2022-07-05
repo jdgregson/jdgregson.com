fcd2f87ca3d01bbcdb3981564624013e51a29409
jdgregson <jdgregson@gmail.com>
2022-07-05 16:32:46 -0700

    Updated vuln list.

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,164 +1,165 @@
 interface VulnListItem {
   type: string;
   url: string;
+  cve: string;
   app: string;
   vendor: string;
   impact: string;
-  class: string;
-  fixed: string;
+  target: string;
 }
 
 const vulns: VulnListItem[] = [
   {
     type: 'Sensitive Information Disclosure',
     url: '',
+    cve: '',
     app: 'Poshmark',
     vendor: 'Poshmark',
     impact: 'P2',
-    class: 'Web, Mobile',
-    fixed: 'NO',
+    target: 'Web, Mobile',
   },
   {
     type: 'Authentication Bypass',
     url: '',
+    cve: '',
     app: 'Outlook for iOS/iPadOS',
     vendor: 'Microsoft',
     impact: 'P3',
-    class: 'Mobile',
-    fixed: 'YES',
+    target: 'Mobile',
   },
   {
     type: 'Privilege Escalation',
     url: 'https://github.com/cloudflare/advisories/security/advisories/GHSA-m6w8-3pf9-p68r',
+    cve: 'CVE-2022-2147',
     app: 'Cloudflare WARP for Windows',
     vendor: 'Cloudflare',
     impact: 'P3',
-    class: 'Desktop',
-    fixed: 'YES',
+    target: 'Desktop',
   },
   {
     type: 'Use of Weak Hash',
     url: '',
+    cve: '',
     app: 'Bitdefender Total Security',
     vendor: 'Bitdefender',
     impact: 'P4',
-    class: 'Desktop',
-    fixed: 'NO',
+    target: 'Desktop',
   },
   {
     type: 'Denial-of-Service',
     url: 'https://github.com/jdgregson/Disclosures/tree/master/microsoft/wdag-dos-long-hostname',
+    cve: '',
     app: 'Application Guard',
     vendor: 'Microsoft',
     impact: 'P3',
-    class: 'Web, Desktop',
-    fixed: 'NO',
+    target: 'Web, Desktop',
   },
   {
     type: 'Authenticated Reflected XSS',
     url: 'https://github.com/jdgregson/Disclosures/tree/master/xfinity/xb3-authenticated-reflected-xss',
+    cve: '',
     app: 'Router Admin Interface',
     vendor: 'Cisco/Technicolor',
     impact: 'P3',
-    class: 'Web',
-    fixed: 'NO',
+    target: 'Web',
   },
   {
     type: 'Denial-of-Service',
     url: 'https://github.com/jdgregson/Disclosures/tree/master/xfinity/xb3-dos-via-large-post-request',
+    cve: '',
     app: 'Router Admin Interface',
     vendor: 'Cisco/Technicolor',
     impact: 'P3',
-    class: 'Web',
-    fixed: 'NO',
+    target: 'Web',
   },
   {
     type: 'Authentication Bypass',
     url: '',
+    cve: '',
     app: 'TaxCloud for BigCommerce',
     vendor: 'TaxCloud',
     impact: 'P1',
-    class: 'Web',
-    fixed: 'YES',
+    target: 'Web',
   },
   {
     type: 'Authentication Bypass',
     url: '',
+    cve: '',
     app: 'Bitdefender Total Security',
     vendor: 'Bitdefender',
     impact: 'P3',
-    class: 'Desktop',
-    fixed: 'NO',
+    target: 'Desktop',
   },
   {
     type: 'Unrestricted File Upload',
     url: 'https://seclists.org/fulldisclosure/2020/Nov/30',
+    cve: '',
     app: 'Fancy Product Designer',
     vendor: 'Fancy Product Designer',
     impact: 'P2',
-    class: 'Web',
-    fixed: 'NO',
+    target: 'Web',
   },
   {
     type: 'Stored XSS via SVG Upload',
     url: 'https://seclists.org/fulldisclosure/2020/Nov/29',
+    cve: '',
     app: 'Fancy Product Designer',
     vendor: 'Fancy Product Designer',
     impact: 'P2',
-    class: 'Web',
-    fixed: 'YES',
+    target: 'Web',
   },
   {
     type: 'Sensitive Information Disclosure',
     url: '',
+    cve: '',
     app: 'Bitdefender Total Security',
     vendor: 'Bitdefender',
     impact: 'P3',
-    class: 'Web, Desktop',
-    fixed: 'YES',
+    target: 'Web, Desktop',
   },
   {
     type: 'Authentication Bypass',
     url: '',
+    cve: '',
     app: 'Malwarebytes Premium',
     vendor: 'Malwarebytes',
     impact: 'P2',
-    class: 'Desktop',
-    fixed: 'YES',
+    target: 'Desktop',
   },
   {
     type: 'Authentication Bypass',
     url: '',
+    cve: '',
     app: 'Bitdefender Mobile Security',
     vendor: 'Bitdefender',
     impact: 'P4',
-    class: 'Mobile',
-    fixed: 'YES',
+    target: 'Mobile',
   },
   {
     type: 'Application-level DoS',
     url: '',
+    cve: '',
     app: 'Bitdefender.com',
     vendor: 'Bitdefender',
     impact: 'P3',
-    class: 'Web',
-    fixed: 'YES',
+    target: 'Web',
   },
 ];
 
 const getVulnRow = (item: VulnListItem): HTMLTableRowElement => {
   const tr = document.createElement('tr');
+  tr.classList.add('vuln-list-row');
   if (item.url) {
     tr.innerHTML += `<td><a href="${item.url}">${item.type}</a></td>`;
   } else {
     tr.innerHTML += `<td>${item.type}</td>`;
   }
   tr.innerHTML += `
+      <td>${item.cve}</td>
       <td>${item.app}</td>
       <td>${item.vendor}</td>
       <td>${item.impact}</td>
-      <td>${item.class}</td>
-      <td>${item.fixed}</td>
+      <td>${item.target}</td>
     `;
   return tr;
 };
